8e3fc7f42111a28e6602417ef960a7e6856bf416
pcrafter <prabhatmani.nitb@gmail.com>
2016-10-11 13:30:28 +0530

    initial commit

---
+++ app/src/main/java/com/pmt/sharenote/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pmt.sharenote.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pmt.sharenote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/pmt/sharenote/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.pmt.sharenote.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pmt.sharenote.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
